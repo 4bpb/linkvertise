463cdc7844491c179df29da9187e36afc97fefd6
4bpb <blakebein@gmail.com>
2021-07-04 13:19:15 -0700

    completed in js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 var fs = require('fs');
 
 
@@ -11,7 +10,21 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
-	if(message.content.startsWith(`https://quizlet.com/`)){
+	if(message.content.startsWith(`https://linkvertise.com/`)){
+        try {
+            let first = message.content.split('=')
+            let second = first[1].split('%')
+            let final = second[0]
+    
+            var buf = Buffer.from(final, 'base64'); // Ta-da
+    
+    
+    
+    
+            message.channel.send('<@'+message.author.id+'>'+'    '+ buf.toString());
+        } catch (error) {
+            message.channel.send('<@'+message.author.id+'>'+' We encountered an error decoding this URL');
+        }
 
 	}
 });
